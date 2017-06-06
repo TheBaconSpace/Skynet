7f8e5d8152ca90d2a9425841950adc11b72d930f
Bacon_Space <thestreamstats@gmail.com>
2017-06-06 09:52:15 -0400

    Create skynet.js

---
+++ skynet.js
@@ -0,0 +1,19 @@
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const config = require('./config.json')
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.username}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+    if (msg.content === '!bot') {
+    msg.reply(`This Bot Was Made By Bacon_Space And it is made in ${discordjs}`);
+    msg.react(":okhand:");
+  } //PlaceHolder
+});
+
+client.login(`${config.token}`);
